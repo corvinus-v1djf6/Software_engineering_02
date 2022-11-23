e12b3eec436b753b29c5e9b63db5d3f817da6f04
Balázs Schmidt <balazs.schmidt2@stud.uni-corvinus.hu>
2022-11-23 21:55:13 +0100

    pascal

---
+++ 09_gyak/wwwroot/jsgyak.js
@@ -0,0 +1,34 @@
+﻿window.onload = () => {
+    console.log("betöltődött")
+}
+
+for (var sor = 0; sor < 10; sor++) {
+    let arr = [];
+    let eredmeny = [];
+    for (var oszlop = 0; oszlop <= sor; oszlop++) {
+        if (oszlop === 0 || oszlop === sor - 1) {
+            arr.push(1);
+        } else {
+            arr.push((eredmeny[sor - 2][oszlop - 1] + eredmeny[sor - 2][oszlop]));
+        }
+    }
+    eredmeny.push(arr);
+    return eredmeny;
+}
+
+
+var faktorialis = function (n) {
+    let er = 1;
+    for (let i = 1; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
+
+var faktorialis2 = (n) => {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * faktorialis2(n - 1)
+    }
+}
---
+++ 09_gyak/wwwroot/minta.js
@@ -0,0 +1,33 @@
+﻿console.log("minta fut!")
+
+var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
+
+var faktoriálisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * faktoriálisR(n - 1)
+    }
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(`${i} : ${faktoriálisR(i)}`)
+}
+
+function számítás() {
+    let n = document.getElementById("nTb").value;
+    let n2 = parseInt(n)
+    if (n2) {
+        let er = faktoriális(n)
+        document.getElementById("eredményDiv").innerText = er;
+    } else {
+        document.getElementById("eredményDiv").innerText = "rossz paraméter";
+    }
+
+}
