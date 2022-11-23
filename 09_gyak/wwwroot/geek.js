bbfa5a0cbc0c2b799a38132c116eb62919c57813
Balázs Schmidt <balazs.schmidt2@stud.uni-corvinus.hu>
2022-11-23 22:22:49 +0100

    viccek

---
+++ 09_gyak/wwwroot/geek.js
@@ -0,0 +1,29 @@
+﻿var viccek;
+
+window.onload = () => {
+    console.log("letöltés")
+}
+
+
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+
+    for (var i = 0; i < viccek.length; i++) {
+        console.log(viccek[i].questionText)
+        let elem = document.createElement("li")
+        elem.innerHTML = viccek[i].questionText
+        ide.appendChild(elem)
+    }
+
+
+}
+
